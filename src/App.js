c9fdfd7a6137f119bd9a71075a87f685665b79f0
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-07-09 19:06:12 +0300

    save login progress

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@ import './App.css';
 import Header from "./components/Header";
 import "./index.css"
 import Footer from './components/Footer';
+import { useState } from 'react';
+import Login from "./components/Login"
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({ name: "", email: "" })
+  const [error, setError] = useState("")
+
+  const Login = details => {
+    console.log(details);
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+  }
+
   return (
     <>
       <Header />
+      {(user.email != "") ? (
+        <div className='welcome'>
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button>Logout</button>
+        </div>)
+        : (
+          <Login />
+        )
+      }
       <div className="container">
         <article>
           <h1>What is Lorem Ipsum? </h1>
