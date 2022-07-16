47859058819820b7963bf2ebcc38030efe508c64
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-07-16 22:27:27 +0300

    validate login form

--- src/components/AuthComponents/LoginForm.js
+++ src/components/AuthComponents/LoginForm.js
@@ -5,9 +5,29 @@ import './loginForm.css'
 
 export default function LoginForm({ error }) {
     const [details, setDetails] = useState({ name: '', password: '' })
+    const [inputError, setInputError] = useState({})
+    const [labelsErrors, setLabelsErrors] = useState({})
 
-    function loginHandler(e){
-        setDetails(state => ({ ...state, [e.target.name]: e.target.value }))
+    function loginHandler(e) {
+        setDetails(state => ({ ...state, [e.target.name]: e.target.value }), labelError(e.target.name, e.target.value))
+    }
+
+    function labelError(name, value) {
+        if (name == "name") {
+            setLabelsErrors(state => ({ ...state, name: !(value.length < 4) }))
+        }
+        if (name == "password") {
+            setLabelsErrors(state => ({ ...state, password: !(value.length < 6) }))
+        }
+    }
+
+    const inputValidate = (e) => {
+        if (e.target.name == "name") {
+            setInputError(state => ({ ...state, name: details.name.length < 4 }))
+        };
+        if (e.target.name == "password") {
+            setInputError(state => ({ ...state, password: details.password.length < 6 }))
+        };
     }
 
     const adminUser = {
@@ -16,14 +36,11 @@ export default function LoginForm({ error }) {
     }
 
     const Login = details => {
-        //console.log(details);
+
         loggedIn(details)
         if (details.email == adminUser.email && details.password == adminUser.password) {
             console.log("admin loged");
-            //   setUser({
-            //     name: details.name,
-            //     email: details.email
-            //   });
+
         } else {
             //   setError("Some Error");
         }
@@ -40,15 +57,25 @@ export default function LoginForm({ error }) {
             <form className='basicForm' onSubmit={submitHandler}>
                 <div className='form-inner'>
                     <h2>Login</h2>
-                    {(error != "") ? (<div className='error'>{error}</div>) : ""}
+                    {/* {(error != "") ? (<div className='error'>{error}</div>) : ""} */}
                     <div className='form-group'>
-                        <label htmlFor="name">Name:</label>
-                        <input type="name" name="name" id="name" onChange={loginHandler} value={details.name} />
+                        <label htmlFor="name" className={labelsErrors.name ? 'basicForm__label-error' : ''}>Name:</label>
+                        <input type="name" name="name" id="name" onChange={loginHandler} value={details.name} onBlur={inputValidate} />
                     </div>
+                    {inputError.name &&
+                        <p className='basicForm__form-error'>
+                            Username must be at least 4 character long.
+                        </p>
+                    }
                     <div className='form-group'>
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" name="password" id="password" onChange={loginHandler} />
+                        <label htmlFor="password" className={labelsErrors.password ? 'basicForm__label-error' : ''}>Password:</label>
+                        <input type="password" name="password" id="password" onChange={loginHandler} onBlur={inputValidate} />
                     </div>
+                    {inputError.password &&
+                        <p className='basicForm__form-error'>
+                            Password must be at least 6 characters long.
+                        </p>
+                    }
                     <input type='submit' value='LOGIN' onClick={submitHandler}></input>
                 </div>
             </form>
