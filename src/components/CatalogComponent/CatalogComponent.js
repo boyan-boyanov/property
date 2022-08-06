9593207fabbf65baf3fd9b30fc528f0548fcf6f2
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-08-06 15:54:39 +0300

    add pagination

--- src/components/CatalogComponent/CatalogComponent.js
+++ src/components/CatalogComponent/CatalogComponent.js
@@ -4,18 +4,25 @@ import CardComponent from '../CardComponent/CardComponent';
 import './catalogComponent.css'
 
 export const CatalogComponent = () => {
-    const [allData, setAllData] = useState([])
+    const [allData, setAllData] = useState([]);
+    const [showData, setShowData] = useState([]);
+    const [page, setPage] = useState({})
+    const numberOfViewItems = 5
 
     useEffect(() => {
         async function waitData() {
             const data = await getAll()
             // console.log(data);
             setAllData(data.results)
+            const show = data.results.slice(0, numberOfViewItems)
+            setShowData(show)
+            const numberOfPages = Math.ceil(data.results.length / numberOfViewItems)
+            setPage({ numberOfPages, currentPage: 1 })
+            console.log(Math.ceil(data.results.length / numberOfViewItems));
+
         }
         waitData()
     }, []);
-
-    
 
 
     function createProps(x) {
@@ -34,11 +41,51 @@ export const CatalogComponent = () => {
         }
     }
 
+    const nextPage = () => {
+
+        if (page.currentPage < page.numberOfPages) {
+            const nextList = page.currentPage + 1
+            setPage(state => ({
+                ...state, currentPage: nextList
+            }))
+            console.log("nextList", nextList);
+            const startPoint = (nextList - 1) * numberOfViewItems
+            console.log("startPoint", startPoint);
+            const endPoint = startPoint + numberOfViewItems
+            console.log("endPoint", endPoint);
+            const nextListOfViews = allData.slice(startPoint, endPoint)
+            setShowData(nextListOfViews)
+        }
+
+    }
+
+    const previewPage = () => {
+        if (page.currentPage > 1) {
+            const previewList = page.currentPage - 1
+            setPage(state => ({
+                ...state, currentPage: previewList
+            }))
+            console.log("previewList", previewList);
+            const startPoint = (previewList - 1) * numberOfViewItems
+            console.log("startPoint", startPoint);
+            const endPoint = startPoint + numberOfViewItems
+            console.log("endPoint", endPoint);
+            const previewListOfViews = allData.slice(startPoint, endPoint)
+            setShowData(previewListOfViews)
+        }
+    }
+
     return (
         <div className={'catalog-container'}>
             <h1>Catalog</h1>
+            <span className='paginator'>
+                <button className='paginator__btn' onClick={previewPage}>&#8810; previews</button>
+                <span className='paginator__text'>{page.currentPage} / {page.numberOfPages}</span>
+                <button className='paginator__btn' onClick={nextPage}>next &#8811;</button>
+
+            </span>
             {allData.length > 0 ?
-                allData.map(x => <CardComponent key={x.objectId} styles={createProps(x)} allId={{ owner: x.Owner, itemId: x.objectId }} />)
+                showData.map(x => <CardComponent key={x.objectId} styles={createProps(x)} allId={{ owner: x.Owner, itemId: x.objectId }} />)
                 : <p className='no-articles'>No Articles yet</p>}
         </div>
     )
